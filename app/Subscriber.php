fac92da4096a99f9cbae08f7c3b0362ed855cf0a
Mateusz Tadeusz <michal1311@o2.pl>
2019-04-26 14:43:45 +0200

    add subscribers model and migration

--- app/Subscriber.php
+++ app/Subscriber.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Subscriber extends Model
 {
     protected $fillable = [
-        'name', 'email',
+        'first_name', 'last_name', 'email',
         'email_verified_at', 'invitation',
-        'got_price', 'event_name'
+        'got_price', 'event_name', 'address',
+        'city', 'post_code',
     ];
 }
--- database/migrations/2019_04_26_074128_create_subscribers_table.php
+++ database/migrations/2019_04_26_074128_create_subscribers_table.php
@@ -15,12 +15,16 @@ class CreateSubscribersTable extends Migration
     {
         Schema::create('subscribers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('invitation')->nullable();
             $table->boolean('got_price')->default(0);
             $table->string('event_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('post_code')->nullable();
             $table->timestamps();
         });
     }
