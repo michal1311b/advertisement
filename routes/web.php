c0f123639e31c3955b4e720e318edbe30b7a49f6
Mateusz Tadeusz <michal1311@o2.pl>
2019-05-09 15:01:53 +0200

    add routes to post push notifications and get them

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::post('/push','PushController@store');
+Route::get('/push','PushController@push')->name('push');
+
 Route::get('/chats', 'ChatsController@index');
 
 Route::get('/messages', 'ChatsController@fetchMessages');
