8447f4d40d8bff0c3af4c4bcc8176a245627d3e4
michal1311 <michal.biernat@lptg.pl>
2019-07-21 20:22:49 +0200

    store advertisement data in DB

--- app/Advertisement.php
+++ app/Advertisement.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Advertisement extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'description', 'work_id', 'slug'
+    ];
+
+    private static function getUniqueSlug($title)
+    {
+        $slug = str_slug($title, '-');
+        $slugCount = count( Work::whereRaw("slug ~ '^{$slug}(-[0-9]*)?$'")->get() );
+        return ($slugCount > 0) ? "{$slug}-{$slugCount}" : $slug;
+    }
 }
--- app/Work.php
+++ app/Work.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Work extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'slug', 'excerpt'
+    ];
 }
