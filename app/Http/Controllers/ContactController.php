0b99b72d24d04b5049fe132da7bd96ac5a3e84bd
michal1311 <michal.biernat@lptg.pl>
2019-08-08 19:38:31 +0200

    store and send email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Contact;
+use App\Http\Service\Mailer;
 
 class ContactController extends Controller
 {
     public function store(Request $request)
     {
-        Contact::create($request->all());
+        $contact = Contact::create($request->all());
+        $send = Mailer::sendEmail(
+            $contact::with('user')
+            ->find($request['user_id'])
+        );
 
         return back();
     }
---
+++ resources/views/emails/question.blade.php
@@ -0,0 +1,2 @@
+There was a question from {{ $contact['first_name'] }}, {{ $contact['phone'] }}:<br><br>
+{!! $contact['message'] !!}
