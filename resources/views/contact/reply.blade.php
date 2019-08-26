fd12b3a37f70b27cb37b919233190b029269c60f
michal1311 <michal.biernat@lptg.pl>
2019-08-26 17:59:10 +0200

    display replies if they exist

--- resources/views/contact/reply.blade.php
+++ resources/views/contact/reply.blade.php
@@ -27,9 +27,21 @@
             <strong>Phone:</strong> {{ $contact->phone }}
             <hr>
             <strong>Message:</strong> {!! $contact->message !!}
+            <hr>
+            @if(count($contact->replies) > 0)
+                <strong>Reply's count:</strong> {{ count($contact->replies) }}<br>
+                @foreach($contact->replies as $reply)
+                    <strong>Email:</strong> {{ $reply->email }}
+                    <br><br>
+                    <strong>Message:</strong> {!! $reply->message !!}
+                    <hr>
+                @endforeach
+            @else
+                No replies
+            @endif
         </div>
         <div class="col-12">
-             @include('partials.reply')
+            @include('partials.reply')
         </div>
     </div>
 </div>
