a28f3f7d9dea3444a687fa16eaa770493d8a8278
Michał Biernat <mbiernat@inovatica.com>
2019-10-18 11:30:35 +0200

    add specialization_user migration

---
+++ database/migrations/2019_10_18_090701_create_specialization_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpecializationUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specialization_user', function (Blueprint $table) {
+            $table->integer('specialization_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+      
+            $table->unique(['specialization_id', 'user_id']);
+            $table->index(['specialization_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specialization_user');
+    }
+}
