258817593f3921785c92f0e4f819bd46234a9a98
Michał Biernat <mbiernat@inovatica.com>
2019-10-18 14:53:31 +0200

    comment Vuejs in app,js

--- resources/js/app.js
+++ resources/js/app.js
@@ -96,7 +96,7 @@ function makeNotificationText(notification) {
 require('./questionnaire');
 
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
 
 /**
@@ -110,7 +110,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 
 /**
@@ -119,6 +119,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
-});
+// const app = new Vue({
+//     el: '#app',
+// });
