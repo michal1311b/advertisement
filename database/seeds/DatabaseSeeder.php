22adb9506baa3600cc22630d8955d4e1aa26456c
Michał Biernat <mbiernat@inovatica.com>
2019-10-18 11:33:20 +0200

    add Specialization Table Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserTableSeeder::class);
         $this->call(ProfileTableSeeder::class);
         $this->call(AdvertisementSeeder::class);
-
+        $this->call(SpecializationTableSeeder::class);
     }
 }
