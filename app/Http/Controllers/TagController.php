589a26a8ade228781f6dd4c82fd85c0c36d60e2a
Michał Biernat <mbiernat@inovatica.com>
2019-10-21 11:00:12 +0200

    display location when seach by tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Tag;
+use App\Location;
 
 class TagController extends Controller
 {
     public function show($slug, $page = 1) {
         $advertisements = Tag::with('advertisement')->where('name', $slug)->paginate(5);
+
+        $locations = Location::all();
         
         if($advertisements) {
-            return view('tag.index', compact('advertisements'));
+            return view('tag.index', compact('advertisements', 'locations'));
         }
     }
   
