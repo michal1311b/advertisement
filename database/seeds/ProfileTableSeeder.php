31040889e6b473da9cd0e9d9e783a80da6f8d839
Michał Biernat <mbiernat@inovatica.com>
2019-10-22 10:20:49 +0200

    create profile for admin in seeder

--- database/seeds/ProfileTableSeeder.php
+++ database/seeds/ProfileTableSeeder.php
@@ -40,5 +40,19 @@ class ProfileTableSeeder extends Seeder
         $profile->company_city = 'Dupa';
         $profile->company_nip = '955555454';
         $profile->save();
+
+        $user3 = User::find(3);
+        $profile = new Profile();
+        $profile->user_id = $user3->id;
+        $profile->last_name = 'Dupa';
+        $profile->street = 'Dupa 12/22';
+        $profile->post_code = '90-500';
+        $profile->city = 'Dupa';
+        $profile->company_name = 'Dupa';
+        $profile->company_street = 'Dupa';
+        $profile->company_post_code = '90-555';
+        $profile->company_city = 'Dupa';
+        $profile->company_nip = '955555454';
+        $profile->save();
     }
 }
