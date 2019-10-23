9b386e8a87de16091c045506ae04a1f95c80ecc0
Michał Biernat <mbiernat@inovatica.com>
2019-10-23 11:32:25 +0200

    add stats migration

---
+++ database/migrations/2019_10_23_071644_create_stats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('url')->nullable();
+            $table->string('host')->nullable();
+            $table->string('action')->nullable();
+            $table->string('title')->nullable();
+            $table->string('email')->nullable();
+            $table->string('session_key')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stats');
+    }
+}
