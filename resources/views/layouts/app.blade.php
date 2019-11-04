cc31e0c23cb7f41cd74ac209de0cd8921efd6e0c
michal1311 <michal.biernat@lptg.pl>
2019-11-04 11:19:27 +0100

    add google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,5 +65,17 @@
     <!-- Scripts -->
     @yield('scripts')
     <script src="{{ asset('js/app.js?rand=1') }}" defer></script>
+
+    @if($app->environment('production'))
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-151388518-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-151388518-1');
+        </script>
+    @endif
 </body>
 </html>
