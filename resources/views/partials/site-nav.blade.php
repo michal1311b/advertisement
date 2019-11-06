dbd792722adced27746fe1cd8e8f6fafc8491410
michal1311 <michal.biernat@lptg.pl>
2019-11-06 17:16:40 +0100

    add blog and advertisement list to page navbar

--- resources/views/partials/site-nav.blade.php
+++ resources/views/partials/site-nav.blade.php
@@ -1,6 +1,12 @@
 
 <div class="list-group list-group-flush">
+    <a href="{{ route('blog.index') }}" class="list-group-item list-group-item-action bg-light">{{ __('Blog') }}</a>
+</div>
+<div class="list-group list-group-flush">
+    <a href="{{ route('advertisement-list') }}" class="list-group-item list-group-item-action bg-light">{{ __('Offers') }}</a>
+</div>
+<div class="list-group list-group-flush">
     @foreach(\App\Page::all() as $page)
-    <a href="{{ route('site.page', $page->slug) }}" class="list-group-item list-group-item-action bg-light">{{ $page->title }}</a>
+        <a href="{{ route('site.page', $page->slug) }}" class="list-group-item list-group-item-action bg-light">{{ $page->title }}</a>
     @endforeach
 </div>
