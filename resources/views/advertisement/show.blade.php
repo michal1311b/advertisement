2f326b0f35385f5bcfcf0881f59e76380847ba83
michal1311 <michal.biernat@lptg.pl>
2019-11-09 22:05:47 +0100

    add social media shares

--- resources/views/advertisement/show.blade.php
+++ resources/views/advertisement/show.blade.php
@@ -211,4 +211,6 @@ for ( var i=0; i < markers.length; ++i )
       .addTo( map );
 }
 </script>
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5dc7278fb46e12ce"></script>
 @endsection
--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -185,5 +185,7 @@
             });
         }
     </script>
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5dc7278fb46e12ce"></script>
 </body>
 </html>
