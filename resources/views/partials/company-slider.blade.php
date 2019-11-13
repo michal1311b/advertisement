50f694ca4c7ed9657a6429adae0fa8c6fc47e8b3
michal1311 <michal.biernat@lptg.pl>
2019-11-13 17:03:31 +0100

    style companies slider, add tooltip

--- resources/views/partials/company-slider.blade.php
+++ resources/views/partials/company-slider.blade.php
@@ -1,8 +1,8 @@
 <div class="customer-logos">
     @if($companies->count())
         @foreach($companies as $company)
-            <div class="slide">
-                <img src="{{ $company->avatar }}" alt="{{ $company->profile->company_name ?? '' }}">
+            <div class="slide" tabindex="0" data-toggle="tooltip" title="{{ $company->profile->company_name ?? '' }}">
+                <img class="slide-image" src="{{ $company->avatar }}" alt="{{ $company->profile->company_name ?? '' }}">
             </div>
         @endforeach
     @endif
