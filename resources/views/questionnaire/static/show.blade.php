df6ecd8aeb0efd305e19ba47ad64d841056a7118
michal1311b <michal1311b@gmail.com>
2019-11-19 15:47:08 +0100

    fix styling

--- resources/views/questionnaire/static/show.blade.php
+++ resources/views/questionnaire/static/show.blade.php
@@ -21,13 +21,13 @@
                         @csrf
                         <div class="form-group">
                             <h5><span class="grey">1</span>{{ trans('sentence.sex') }}</h5>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="kobieta" id="female" name="sex">
                                 <label class="form-check-label radio-label" for="female">
                                     {{ trans('sentence.female') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="mężczyzna" id="male" name="sex">
                                 <label class="form-check-label radio-label" for="male">
                                     {{ trans('sentence.male') }}
@@ -37,37 +37,37 @@
 
                         <div class="form-group">
                             <h5><span class="grey">2</span>{{ trans('sentence.age') }}</h5>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="Do 25" id="age1" name="age">
                                 <label class="form-check-label radio-label" for="age1">
                                     {{ trans('sentence.age1') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="26-35" id="age2" name="age">
                                 <label class="form-check-label radio-label" for="age2">
                                     {{ __('26-35') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="36-45" id="age3" name="age">
                                 <label class="form-check-label radio-label" for="age3">
                                     {{ __('36-45') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="46-55" id="age4" name="age">
                                 <label class="form-check-label radio-label" for="age4">
                                     {{ __('46-55') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="56-65" id="age5" name="age">
                                 <label class="form-check-label radio-label" for="age5">
                                     {{ __('56-65') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value=">65" id="age6" name="age">
                                 <label class="form-check-label radio-label" for="age6">
                                     {{ __('>65') }}
@@ -117,31 +117,31 @@
 
                         <div class="form-group">
                             <h5><span class="grey">5</span>{{ trans('sentence.workplace') }}</h5>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="Szpital" id="hospital" name="workplace">
                                 <label class="form-check-label radio-label" for="hospital">
                                     {{ trans('sentence.hospital') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="Poradnia specjalistyczna" id="specialist-outpatient-clinic" name="workplace">
                                 <label class="form-check-label radio-label" for="specialist-outpatient-clinic">
                                     {{ trans('sentence.specialist-outpatient-clinic') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="POZ" id="poz" name="workplace">
                                 <label class="form-check-label radio-label" for="poz">
                                     {{ __('POZ') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="Prywatna praktyka lekarska" id="private-medical-practice" name="workplace">
                                 <label class="form-check-label radio-label" for="private-medical-practice">
                                     {{ trans('sentence.private-medical-practice') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <label class="form-check-label radio-label" for="something-else">
                                     {{ trans('sentence.something-else') }}
                                 </label>
@@ -151,25 +151,25 @@
 
                         <div class="form-group">
                             <h5><span class="grey">6</span>{{ trans('sentence.time-to-find-job') }}</h5>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="5-10 min" id="worktime1" name="worktime">
                                 <label class="form-check-label radio-label" for="worktime1">
                                     {{ __('5-10 min') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="10-30 min" id="worktime2" name="worktime">
                                 <label class="form-check-label radio-label" for="worktime2">
                                     {{ __('10-30 min') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="30-60 min" id="worktime3" name="worktime">
                                 <label class="form-check-label radio-label" for="worktime3">
                                     {{ __('30-60 min') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="Powyżej 60 min" id="worktime4" name="worktime">
                                 <label class="form-check-label radio-label" for="worktime4">
                                     {{ trans('sentence.worktime4') }}
@@ -209,31 +209,31 @@
 
                         <div class="form-group">
                             <h5><span class="grey">9</span>{{ trans('sentence.social-media-quiestion') }}</h5>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="Facebook" id="facebook" name="social_media">
                                 <label class="form-check-label radio-label" for="facebook">
                                     {{ __('Facebook') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="Serwisy pośrednictwa pracy" id="job-placement-services" name="social_media">
                                 <label class="form-check-label radio-label" for="job-placement-services">
                                     {{ trans('sentence.job-placement-services') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="Prasa branżowa" id="industry-press" name="social_media">
                                 <label class="form-check-label radio-label" for="industry-press">
                                     {{ trans('sentence.industry-press') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <input class="form-check-input" type="radio" value="Konsylium" id="consultation" name="social_media">
                                 <label class="form-check-label radio-label" for="consultation">
                                     {{ trans('sentence.consultation') }}
                                 </label>
                             </div>
-                            <div class="form-check radio">
+                            <div class="form-check radio px-1">
                                 <label class="form-check-label radio-label" for="something-else">
                                     {{ trans('sentence.something-else') }}
                                 </label>
