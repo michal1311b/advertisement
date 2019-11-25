e608192676a7ddc8a2ef6eb1007e1db1a5881c69
michal1311b <michal1311b@gmail.com>
2019-11-25 12:12:48 +0100

    fix cookies link

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -196,7 +196,7 @@
         };
     </script>
 
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
     <!-- End Cookie Consent plugin -->
 </body>
 </html>
