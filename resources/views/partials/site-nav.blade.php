f3d0c4dc59224a455c60d001bd181498556634b4
michal1311b <michal1311b@gmail.com>
2019-11-26 09:10:26 +0100

    add contact form to side bar

--- resources/views/partials/site-nav.blade.php
+++ resources/views/partials/site-nav.blade.php
@@ -9,6 +9,9 @@
     <a href="{{ route('company-list') }}" class="list-group-item list-group-item-action bg-light">{{ trans('sentence.company-list') }}</a>
 </div>
 <div class="list-group list-group-flush">
+    <a href="{{ route('contact.show') }}" class="list-group-item list-group-item-action bg-light {{ Request::is('kontakt') ? 'text-primary active' : null }}">{{ trans('sentence.contact-form') }}</a>
+</div>
+<div class="list-group list-group-flush">
     @foreach(\App\Page::where('is_active', 1)->get() as $page)
         <a href="{{ route('site.page', $page->slug) }}" class="list-group-item list-group-item-action bg-light {{ Request::is($page->slug) ? 'text-primary active' : null }}">{{ $page->title }}</a>
     @endforeach
