8c34845d1bcb430d280166cbcc220a8be8d91296
michal1311b <michal1311b@gmail.com>
2019-11-26 10:05:59 +0100

    add time to block contact form

--- config/honeypot.php
+++ config/honeypot.php
@@ -28,7 +28,7 @@ return [
      * If the form is submitted faster then this amount of seconds
      * the form submission will be considered invalid.
      */
-    'amount_of_seconds' => env('HONEYPOT_SECONDS', 1),
+    'amount_of_seconds' => env('HONEYPOT_SECONDS', 30),
 
     /*
      * This class is responsible for sending a response to request that
