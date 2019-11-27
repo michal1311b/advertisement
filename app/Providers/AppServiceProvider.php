b47f10d280a210ff94863924d6d435d89bcec84a
michal1311b <michal1311b@gmail.com>
2019-11-27 12:32:44 +0100

    Customizing email verification notification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,11 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use App\Observers\PostObserver;
 use App\Advertisement;
+use Carbon\Carbon;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -37,6 +42,24 @@ class AppServiceProvider extends ServiceProvider
     
         \Validator::replacer('greater_than_field', function($message, $attribute, $rule, $parameters) {
             return str_replace(':field', $parameters[0], $message);
+        });
+
+        VerifyEmail::toMailUsing(function ($notifiable){
+            $verifyUrl = URL::temporarySignedRoute(
+                'verification.verify',
+                Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+                ['id' => $notifiable->getKey()]
+            );
+
+            return (new MailMessage())
+            ->subject('EmployMed - Email verification')
+            ->greeting('Witaj,')
+            ->line('Aktywuj swoje konto w serwisie EmployMed.eu, klikając w przycisk poniżej.')
+            ->action('Kliknij, aby aktywować konto', $verifyUrl)
+            ->line('Ten email został wygenerowany automatycznie. Jeśli nie zakładałeś konta w serwisie, prosimy o kontakt na adres')
+            ->line('contactemploymed@gmail.com')
+            ->line('w celu jego usunięcia z naszej bazy. Dziękujemy.')
+            ->salutation('Pozdrawiamy - Zespół EmployMed');
         });
     }
 }
