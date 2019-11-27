436bf5a01e708d885d4071479fb9eb7c560504d9
michal1311b <michal1311b@gmail.com>
2019-11-27 15:30:12 +0100

    style edit, create offer views

--- resources/views/advertisement/create.blade.php
+++ resources/views/advertisement/create.blade.php
@@ -333,7 +333,7 @@
                                 </div>
 
                                 <div class="form-group row mb-0">
-                                    <div class="col-md-8 offset-md-4">
+                                    <div class="col-12">
                                         <button type="submit" class="btn btn-primary">
                                             {{ trans('sentence.btn-create') }}
                                         </button>
--- resources/views/advertisement/edit.blade.php
+++ resources/views/advertisement/edit.blade.php
@@ -306,7 +306,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-12">
                                 <button type="submit" class="btn btn-success">
                                     {{ trans('sentence.btn-update') }}
                                 </button>
