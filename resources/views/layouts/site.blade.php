b91fcdf70a350fabd7d70cd2d89aababb4a5c34c
michal1311b <michal1311b@gmail.com>
2019-12-07 17:37:42 +0100

    add hotjar

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -189,6 +189,17 @@
                 });
             });
         </script>
+        <!-- Hotjar Tracking Code for https://employmed.eu -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:1603858,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
     @endif
     <!-- Go to www.addthis.com/dashboard to customize your tools -->
     <script type="text/javascript" src="https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5dc7278fb46e12ce"></script>
