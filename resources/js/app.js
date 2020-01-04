1e589fefdd53552720f9936d15e57999a0c4c3ad
michal1311b <michal1311b@gmail.com>
2020-01-04 17:44:43 +0100

    load dropzone in edit vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -265,52 +265,57 @@ $(document).ready(function() {
 
     $(".blue-tooltip").tooltip({});
 
-    window.Dropzone = require('dropzone');
-    Dropzone.autoDiscover = false;
-
-    Dropzone.options.dropzone = {
-        previewTemplate: document.getElementById('template-preview').innerHTML,
-        previewsContainer: '.dropzone-previews',
-        createImageThumbnails: true,
-        thumbnailHeight: 120,
-        thumbnailWidth: 120,
-        thumbnail: function(file, dataUrl) {
-            if (file.previewElement) {
-                file.previewElement.classList.remove("dz-file-preview");
-                var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
-                for (var i = 0; i < images.length; i++) {
-                    var thumbnailElement = images[i];
-                    thumbnailElement.alt = file.name;
-                    thumbnailElement.src = dataUrl;
-                }
-                setTimeout(function() { file.previewElement.classList.add("dz-image-preview"); }, 1);
-            }
-        },
-        maxFilesize: 12,
-            renameFile: function(file) {
-            var dt = new Date();
-            var time = dt.getTime();
-            return time+file.name;
-        },
-        acceptedFiles: ".pdf,.doc,.docx",
-        addRemoveLinks: true,
-        timeout: 50000,
-        removedfile: function(file) {
-            var name = file.upload.filename;
-            var fileRef;
-            return (fileRef = file.previewElement) != null ? 
-            fileRef.parentNode.removeChild(file.previewElement) : void 0;
-        },
+    var text = window.location.href;
     
-        success: function(file, response) 
-        {
-            console.log(response);
-        },
-        error: function(file, response)
-        {
-            return false;
-        }
-    };
+    if(text.includes('user') && text.includes('edit'))
+    {
+        window.Dropzone = require('dropzone');
+        Dropzone.autoDiscover = false;
+
+        Dropzone.options.dropzone = {
+            previewTemplate: document.getElementById('template-preview').innerHTML,
+            previewsContainer: '.dropzone-previews',
+            createImageThumbnails: true,
+            thumbnailHeight: 120,
+            thumbnailWidth: 120,
+            thumbnail: function(file, dataUrl) {
+                if (file.previewElement) {
+                    file.previewElement.classList.remove("dz-file-preview");
+                    var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
+                    for (var i = 0; i < images.length; i++) {
+                        var thumbnailElement = images[i];
+                        thumbnailElement.alt = file.name;
+                        thumbnailElement.src = dataUrl;
+                    }
+                    setTimeout(function() { file.previewElement.classList.add("dz-image-preview"); }, 1);
+                }
+            },
+            maxFilesize: 12,
+                renameFile: function(file) {
+                var dt = new Date();
+                var time = dt.getTime();
+                return time+file.name;
+            },
+            acceptedFiles: ".pdf,.doc,.docx",
+            addRemoveLinks: true,
+            timeout: 50000,
+            removedfile: function(file) {
+                var name = file.upload.filename;
+                var fileRef;
+                return (fileRef = file.previewElement) != null ? 
+                fileRef.parentNode.removeChild(file.previewElement) : void 0;
+            },
+        
+            success: function(file, response) 
+            {
+                console.log(response);
+            },
+            error: function(file, response)
+            {
+                return false;
+            }
+        };
+    }
 });
 
 var minSteps = 6,
@@ -318,33 +323,38 @@ maxSteps = 60,
 timeBetweenSteps = 100,
 bytesPerStep = 100000;
 
-dropzone.uploadFiles = function(files) {
-    var self = this;
+var text = window.location.href;
+    
+if(text.includes('user') && text.includes('edit'))
+{
+    dropzone.uploadFiles = function(files) {
+        var self = this;
 
-    for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
 
-        for (var step = 0; step < totalSteps; step++) {
-            var duration = timeBetweenSteps * (step + 1);
-            setTimeout(function(file, totalSteps, step) {
-                return function() {
-                    file.upload = {
-                        progress: 100 * (step + 1) / totalSteps,
-                        total: file.size,
-                        bytesSent: (step + 1) * file.size / totalSteps
+            for (var step = 0; step < totalSteps; step++) {
+                var duration = timeBetweenSteps * (step + 1);
+                setTimeout(function(file, totalSteps, step) {
+                    return function() {
+                        file.upload = {
+                            progress: 100 * (step + 1) / totalSteps,
+                            total: file.size,
+                            bytesSent: (step + 1) * file.size / totalSteps
+                        };
+
+                        self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
+                        if (file.upload.progress == 100) {
+                            file.status = Dropzone.SUCCESS;
+                            self.emit("success", file, 'success', null);
+                            self.emit("complete", file);
+                            self.processQueue();
+                            //document.getElementsByClassName("dz-success-mark").style.opacity = "1";
+                        }
                     };
-
-                    self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
-                    if (file.upload.progress == 100) {
-                        file.status = Dropzone.SUCCESS;
-                        self.emit("success", file, 'success', null);
-                        self.emit("complete", file);
-                        self.processQueue();
-                        //document.getElementsByClassName("dz-success-mark").style.opacity = "1";
-                    }
-                };
-            }(file, totalSteps, step), duration);
+                }(file, totalSteps, step), duration);
+            }
         }
     }
 }
