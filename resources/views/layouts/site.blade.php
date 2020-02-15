ae881938d3e244826206c90f18fc80fa9591946e
michal1311b <michal1311b@gmail.com>
2020-02-15 12:10:16 +0100

    build for production

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -42,7 +42,7 @@
     <script src="https://kit.fontawesome.com/96c3aa2e82.js" crossorigin="anonymous"></script>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css?rand=27') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?rand=28') }}" rel="stylesheet">
     @yield('css')
       
     <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js'></script>
@@ -92,7 +92,7 @@
         </a>
     </div>
     @include('partials.footer')
-    <script src="{{ asset('js/app.js?rand=27') }}" defer></script>
+    <script src="{{ asset('js/app.js?rand=28') }}" defer></script>
     
     <!-- Scripts -->
     @yield('scripts')
