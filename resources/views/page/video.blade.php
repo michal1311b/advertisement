b2210cee8a825a5f0f997e9900f637088324f461
michal1311b <michal1311b@gmail.com>
2020-03-20 10:48:23 +0100

    change ScaleDrone to own

--- resources/views/page/video.blade.php
+++ resources/views/page/video.blade.php
@@ -58,7 +58,7 @@ if (!location.hash) {
 const roomHash = location.hash.substring(1);
 
 // TODO: Replace with your own channel ID
-const drone = new ScaleDrone('yiS12Ts5RdNhebyM');
+const drone = new ScaleDrone('x2uvlaWtJHHgTxGF');
 // Room name needs to be prefixed with 'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
