563e0e3cc9a419abbd01141c6c7dfa57bc1bf240
michal1311b <michal1311b@gmail.com>
2020-03-23 15:31:33 +0100

    change height of selectpickers

--- resources/js/app.js
+++ resources/js/app.js
@@ -232,25 +232,31 @@ $(document).ready(function() {
     $('#category_id').selectpicker();
     $('#mailinglist_id').selectpicker();
     $('#location_id').selectpicker({
-        width: '100%'
+        width: '100%',
+        size: 5
     });
     $('#user_location_id').selectpicker({
-        width: '100%'
+        width: '100%',
+        size: 5
     });
     $('#specialization_id').selectpicker({
-        width: '100%'
+        width: '100%',
+        size: 5
     });
     $('#location_ids').selectpicker({
-        width: 'fit'
+        width: 'fit',
+        size: 5
     });
     $('#specialization_ids').selectpicker({
-        width: 'fit'
+        width: 'fit',
+        size: 5
     });
     $('#currency_ids').selectpicker({
         width: 'fit'
     });
     $('#state_ids').selectpicker({
-        width: 'fit'
+        width: 'fit',
+        size: 5
     });
 
     $(function () {
