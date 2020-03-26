1a5edf3a2d1272a78804b1662d0a75b5cf6fffe0
michal1311b <michal1311b@gmail.com>
2020-03-26 13:52:36 +0100

    rebuild search bar in offers

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,13 +1,9 @@
-<div class="col-12">
-    <form action="{{ route('search-advertisement') }}" method="GET" role="search">
-        {{ csrf_field() }}
-        <div class="form-row">
-            <div class="form-group">
-                <div class="input-group search-panel mb-3">
-                    <div class="input-group-prepend">
-                        <button class="btn btn-outline-secondary" type="submit" id="button-addon1">{{ trans('sentence.search') }}</button>
-                        <a href="{{ route('advertisement-list') }}" class="btn btn-outline-secondary" id="button-addon2">{{ trans('sentence.clear-search') }}</a>
-                    </div>
+<form action="{{ route('search-advertisement') }}" method="GET" role="search">
+    {{ csrf_field() }}
+    <div class="card">
+        <article class="card-group-item">
+            <div class="filter-content">
+                <div class="card-body">
                     <select data-live-search="true" class="form-control @error('specialization_id') is-invalid @enderror" name="specialization_id" id="specialization_ids">
                         <option selected value="">{{ trans('sentence.choose-spec') }}</option>
                         @foreach($specializations as $specialization)
@@ -26,6 +22,13 @@
                             <option value="{{ $state->id }}">{{ $state->name }}</option>
                         @endforeach
                     </select>
+                </div> <!-- card-body.// -->
+            </div>
+        </article> <!-- card-group-item.// -->
+
+        <article class="card-group-item">
+            <div class="filter-content">
+                <div class="card-body">
                     <div class="input-group-append mx-3">
                         <div class="form-group">
                             <input
@@ -35,18 +38,23 @@
                             <label for="range" class="ml-3">
                                 {{ trans('sentence.salary') }} <span id="val1" class="font-weight-bold"></span> - <span id="val2" class="font-weight-bold"></span>
                             </label>
+
+                            <select data-live-search="true" class="form-control @error('currency_id') is-invalid @enderror" name="currency_id" id="currency_ids">
+                                @foreach($currencies as $currency)
+                                    <option @if($currency->id === 1) selected @endif
+                                        value="{{ $currency->id }}">
+                                        {{ $currency->symbol }}
+                                    </option>
+                                @endforeach
+                            </select>
+                            <div class="btn-group" role="group" aria-label="Basic example">
+                                <button class="btn btn-outline-secondary" type="submit" id="button-addon1">{{ trans('sentence.search') }}</button>
+                                <a href="{{ route('advertisement-list') }}" class="btn btn-outline-secondary" id="button-addon2">{{ trans('sentence.clear-search') }}</a>
+                            </div>
                         </div>
                     </div>
-                    <select data-live-search="true" class="form-control @error('currency_id') is-invalid @enderror" name="currency_id" id="currency_ids">
-                        @foreach($currencies as $currency)
-                            <option @if($currency->id === 1) selected @endif
-                                value="{{ $currency->id }}">
-                                {{ $currency->symbol }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
+                </div> <!-- card-body.// -->
             </div>
-        </div>
-    </form>
-</div>
+        </article> <!-- card-group-item.// -->
+    </div> <!-- card.// -->
+</form>
