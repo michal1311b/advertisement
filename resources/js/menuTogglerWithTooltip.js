2e81c16d30f93dceafe8ec9ad5e839700fd2cd29
michal1311b <michal1311b@gmail.com>
2020-04-04 13:54:30 +0200

    close nabcar collapse menu when clicking outside

--- resources/js/menuTogglerWithTooltip.js
+++ resources/js/menuTogglerWithTooltip.js
@@ -12,3 +12,11 @@ $('#envelope').hover(function () {
     $(this).removeClass('fa-envelope-open-text');
     $(this).addClass('fa-envelope');
 });
+
+$(document).click(function (event) {
+    var clickover = $(event.target);
+    var _opened = $(".navbar-collapse").hasClass("show");
+    if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+        $(".navbar-toggler").click();
+    }
+});
