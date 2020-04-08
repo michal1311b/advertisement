d7b2eadf49dceeefaead13a300116963b2278b78
michal1311b <michal1311b@gmail.com>
2020-04-08 15:00:10 +0200

    change translate

--- resources/lang/de/live.php
+++ resources/lang/de/live.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title' => 'Rekrutierung in polnischer Medizin Von Ärzten, Zahnärzten, Krankenschwestern, für Ärzte, Zahnärzte, Krankenschwestern.',
+    'title' => 'Rekrutierung in der polnischen Medizinindustrie Von Ärzten, Zahnärzten, Krankenschwestern, für Ärzte, Zahnärzte, Krankenschwestern.',
     'description' => 'Die aktuelle Situation hat zu einer jahrelangen Vernachlässigung der Gesundheitsversorgung in Polen geführt.<br><br>Als innovatives Portal konzentrieren wir uns auf Transparenz. Unsere Mission war es immer, der polnischen Medizinindustrie zu helfen, einen besseren Arbeitsplatz zu finden, sowie eine medizinische Einrichtung, um den Personalmangel zu beheben.<br><br>Werbung auf Portalen ist eine Sache, aber unser Ziel ist es, Informationen darüber zu sammeln, wer auf dem polnischen Medizinmarkt an einem Ort rekrutiert. Lassen Sie mich daher über das unten stehende Formular wissen, wie es derzeit in Ihrem Unternehmen aussieht.<br><br>Helfen wir allen, die gerade einen Job suchen.#EmployMed.eu',
     'situation' => 'Wie ist die Rekrutierung in Ihrem Unternehmen?',
     'footer-info' => 'Von Medizinern zu Medizinern - ein Board, das von Benutzern erstellt wird',
--- resources/lang/pl/live.php
+++ resources/lang/pl/live.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title' => 'Rekrutacje w polskiej medycznej Od lekarzy, stomatologów, pielęgniarek, dla lekarzy, stomatologów, pielęgniarek.',
+    'title' => 'Rekrutacje w polskiej branży medycznej Od lekarzy, stomatologów, pielęgniarek, dla lekarzy, stomatologów, pielęgniarek.',
     'description' => 'Obecna sytuacja sprawiła, że okazały się wieloletnie zaniedbania w służbie zdrowia w Polsce.<br><br>
     Jako innowacyjny portal stawiamy na transparentność, a naszą misją od zawsze jest pomaganie polskiej branży medycznej w znalezieniu lepszej pracy, a także placówką medycznym w uzupełnieniu braków kadrowych.<br><br>
     Ogłoszenia na portalach to jedno, ale naszym celem jest zebranie informacji o tym, kto rekrutuje na całym polskim rynku medycznym w jednym miejscu - dlatego daj znać, jak to teraz wygląda w Twojej firmie, bezpośrednio przez formularz poniżej.<br><br>
