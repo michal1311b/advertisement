33bae5ed8f9f50ad3998b2802f2adca73d37d7ca
michal1311b <michal1311b@gmail.com>
2020-04-08 21:17:49 +0200

    change live content

--- resources/lang/pl/live.php
+++ resources/lang/pl/live.php
@@ -2,10 +2,7 @@
 
 return [
     'title' => 'Rekrutacje w polskiej branży medycznej Od lekarzy, stomatologów, pielęgniarek, dla lekarzy, stomatologów, pielęgniarek.',
-    'description' => 'Obecna sytuacja sprawiła, że okazały się wieloletnie zaniedbania w służbie zdrowia w Polsce.<br><br>
-    Jako innowacyjny portal stawiamy na transparentność, a naszą misją od zawsze jest pomaganie polskiej branży medycznej w znalezieniu lepszej pracy, a także placówką medycznym w uzupełnieniu braków kadrowych.<br><br>
-    Ogłoszenia na portalach to jedno, ale naszym celem jest zebranie informacji o tym, kto rekrutuje na całym polskim rynku medycznym w jednym miejscu - dlatego daj znać, jak to teraz wygląda w Twojej firmie, bezpośrednio przez formularz poniżej.<br><br>
-    Pomóżmy każdemu, kto obecnie szuka pracy lub chce uzupełnić brak w placówce.#EmployMed.eu',
+    'description' => 'Obecna epidemiadobitnie pokazała, że polski system ochrony zdrowia to kolos na glinianych nogach.<br><br>Jako innowacyjny portal stawiamy na przede wszystkim na transparentność.  Naszą misją od początku jest pomaganie w łączeniu personelu medycznego szukającego pracy z placówkami.<br><br>Ogłoszenia na portalach to jedno ale naszym celem również jest śledzenie trendów na medycznym rynku pracy i podążanie za nimi.<br><br>Chcemy Cię prosić o anonimowe wyrażenie opinii o sytuacji w Twoim zakładzie pracy bezpośrednio przez formularz poniżej.<br><br>Po końcu epidemii planujemy stworzyć raport o stanie zatrudnienia lekarzy i lekarzy dentystów w naszym kraju. #EmployMed.eu',
     'situation' => 'Jak wygląda rekrutacja w Twojej firmie?',
     'footer-info' => 'Od medyków dla medyków - tablica generowana przez użytkowników',
     'status' => 'Status',
